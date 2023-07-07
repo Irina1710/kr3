414127eb08ebeeafc2d74a7ce7dfefd5185a8af2
Irina <irina_sergeeva_12@mail.ru>
2023-07-07 22:01:14 +0500

    fix2

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ def main():
     COUNT_LAST_VALUES = 5
     data = get_data()
     data = get_filtered_data(data)
-    data - get_last_values(data, count_last_values=COUNT_LAST_VALUES)
+    data = get_last_values(data, count_last_values=COUNT_LAST_VALUES)
     data = get_formatted_data(data)
 
     print('INFO: Вывод транзакци...')
